3ec6009b33ed1959722eccf4ce60439221407512
Footsoreorchid7 <50101535+Footsoreorchid7@users.noreply.github.com>
2023-08-24 12:57:19 +0200

    Initial commit

---
+++ src/components/Friend.vue
@@ -0,0 +1,23 @@
+<template>
+  <h3>{{ name }}</h3>
+  <p>
+    <strong>Phone:</strong>
+    {{ phone }}
+  </p>
+  <p>
+    <strong>Email:</strong>
+    {{ mail }}
+  </p>
+  <button @click="$emit('elimina', id)">Elimina</button>
+</template>
+
+<script>
+export default {
+  props: ["id", "name", "phone", "mail"],
+  emits: ["elimina"],
+  name: "Friend",
+  return: {
+    methods: {},
+  },
+};
+</script>
